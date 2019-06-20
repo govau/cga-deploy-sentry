59034994976c2c1162a03d88e71094ca42ebb381
Brendan Gordon <brendan.gordon@digital.gov.au>
2019-06-20 18:04:18 +1000

    Remove ecr secrets as its not used anymore

--- ci/set-secrets.sh
+++ ci/set-secrets.sh
@@ -9,15 +9,6 @@ set -euo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-function trim_to_one_access_key() {
-    iam_user=$1
-    key_count=$(aws iam list-access-keys --user-name "${iam_user}" | jq '.AccessKeyMetadata | length')
-    if [[ $key_count > 1 ]]; then
-        oldest_key_id=$(aws iam list-access-keys --user-name "${iam_user}" | jq -r '.AccessKeyMetadata |= sort_by(.CreateDate) | .AccessKeyMetadata | first | .AccessKeyId')
-        aws iam delete-access-key --user-name "${iam_user}" --access-key-id "${oldest_key_id}"
-    fi
-}
-
 set_credhub_value() {
   KEY="$1"
   VALUE="$2"
@@ -38,24 +29,6 @@ echo "Ensuring you are logged in to credhub"
 if ! https_proxy=socks5://localhost:8112 credhub find > /dev/null; then
   https_proxy=socks5://localhost:8112 credhub login --sso
 fi
-
-echo "Setting iam user creds used to access ECR"
-iam_user=${PIPELINE}-ecr-pusher
-export AWS_PROFILE=l-cld
-trim_to_one_access_key $iam_user
-output="$(aws iam create-access-key --user-name ${iam_user})"
-aws_access_key_id="$(echo $output | jq -r .AccessKey.AccessKeyId)"
-aws_secret_access_key="$(echo $output | jq -r .AccessKey.SecretAccessKey)"
-aws_repository="$(aws ecr describe-repositories | jq -r '.repositories[] | select( .repositoryName == "sentry") | .repositoryUri')"
-
-export https_proxy=socks5://localhost:8112
-credhub s -n /concourse/apps/${PIPELINE}/aws_access_key_id --type value --value "${aws_access_key_id}"
-credhub s -n /concourse/apps/${PIPELINE}/aws_secret_access_key --type value --value "${aws_secret_access_key}"
-credhub s -n /concourse/apps/${PIPELINE}/aws_repository --type value --value "${aws_repository}"
-unset https_proxy
-
-trim_to_one_access_key $iam_user
-unset AWS_PROFILE
 
 echo "Ensuring google auth secrets are set"
 GOOGLE_CREDS_FILE="$SCRIPT_DIR/../google_client_secret.json"
